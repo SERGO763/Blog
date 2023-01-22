32fdf5e70202ead9a3376a6a065e1e0307f2e689
SERGO763 <107487137+SERGO763@users.noreply.github.com>
2023-01-22 12:40:36 +0400

    save arezzo files

--- blog/settings.py
+++ blog/settings.py
@@ -34,6 +34,10 @@ INSTALLED_APPS = [
     # Мои приложения.
     'blogs',
     'users',
+
+    # Сторонние приложения
+    'bootstrap4',
+
     # Приложения django по умолчанию.
     'django.contrib.admin',
     'django.contrib.auth',
@@ -125,3 +129,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Мои настройки
+LOGIN_URL = '/users/login/'
--- blogs/views.py
+++ blogs/views.py
@@ -1,25 +1,36 @@
 from django.shortcuts import render, redirect
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
-# Create your views here.
+# Создайте здесь свои представления.
 def index(request):
     """Домашняя страница приложения Blog"""
     return render(request, 'blogs/index.html')
 
+def check_topic_owner(request, topic):
+    """Проверяет, что пользователь, связанный с темой, является текущем пользователем"""
+    if topic.owner != request.user:
+        raise Http404
+
+@login_required
 def topics(request):
     """Выводит список тем"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'blogs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """Выводит одну тему и все ее записи."""
     topic = Topic.objects.get(id=topic_id)
+    #check_topic_owner(request, topic)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'blogs/topic.html', context)
 
+@login_required
 def new_topic(request):
     """Определяет новую тему"""
     if request.method != 'POST':
@@ -29,16 +40,19 @@ def new_topic(request):
         # Отправлены данные POST; обработать данные.
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('blogs:topics')
 
     # Вывести пустую или нействительную форму
     context = {'form' : form}
     return render(request, 'blogs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """Добавляет новую запись"""
-    topic  = Topic.objects.get(id=topic_id)
+    topic = Topic.objects.get(id=topic_id)
     if request.method != 'POST':
         # Данные не отправлялись; создается пустая форма
         form = EntryForm()
@@ -55,10 +69,12 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'blogs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """Редактирует существующюю запись"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    #check_topic_owner(request, topic)
 
     if request.method != 'POST':
         # Исходный запрос; форма заполнется данными текущей записи.
